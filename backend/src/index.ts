bbe84de24ba28d2cf735be65810dd32a2bde71cb
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-09-12 22:00:58 +0530

    init:scaffold backend with express and prisma

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
