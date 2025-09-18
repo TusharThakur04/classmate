d56d272cc119fd97b83bf3d87a8c01c1ec659bbc
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-09-18 23:16:25 +0530

    feat: add initial storing functionality for flowRuns and flowRunOutbo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,17 @@
 import express from "express";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
+import cors from "cors";
+import flow from "./routes/flows"
 
-// dotenv.config();
+dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("hi");
 });
+app.use("/flows",flow)
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
---
+++ backend/src/routes/flows.ts
@@ -0,0 +1,26 @@
+import Router from "express";
+import { PrismaClient } from "@prisma/client";
+
+const router = Router();
+const prisma = new PrismaClient();
+
+router.post("/:userid/:flowid", async (req, res) => {
+  const data = req.body;
+  const { userid, flowid } = req.params;
+
+  await prisma.$transaction(async (tx) => {
+    const flowRun = await tx.flowRun.create({
+      data: { flowId: flowid },
+    });
+    const flowRunOutbox = await tx.flowRunOutbox.create({
+      data: {
+        flowRunId: flowRun.id,
+      },
+    });
+    return { flowRun, flowRunOutbox };
+  });
+
+  res.json({ message: `flow created`, data });
+});
+
+export default router;
