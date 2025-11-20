7710dc45077c036dfb0c89e81df4bd5284b27df2
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-11-20 21:56:00 +0530

    feat:update actionNode and triggerNode toggle functionality

--- frontend/components/TriggerNode.tsx
+++ frontend/components/TriggerNode.tsx
@@ -1,13 +1,74 @@
+import { useState } from 'react';
 import { Handle, Position } from '@xyflow/react';
-import { useCallback } from 'react';
 
 export function TriggerNode(props) {
+  const [open, setOpen] = useState(false);
+  const [trigger, setTrigger] = useState(false);
+
   return (
-    <div className="text-updater-node flex h-9 w-35 items-center justify-center rounded border border-gray-500 bg-white p-2 shadow-lg">
-      <div className="">
-        trigger
-        <Handle type="source" position={Position.Bottom} />
+    <div className="relative h-10 w-35 rounded border border-gray-500 bg-white p-2 shadow-lg">
+      <div className="flex h-full w-full items-center justify-between px-1">
+        {trigger ? (
+          <>
+            <span className="text-sm font-medium text-gray-800">Gmail Trigger</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="19"
+              height="19"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-trash-icon lucide-trash cursor-pointer text-gray-500 transition hover:scale-95"
+              onClick={() => setTrigger((prev) => !prev)}
+            >
+              <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+              <path d="M3 6h18" />
+              <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+            </svg>
+          </>
+        ) : (
+          <>
+            <span className="text-sm font-medium text-gray-800">Add Trigger</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              onClick={() => setOpen((prev) => !prev)}
+              className="h-[20px] w-[30px] cursor-pointer text-gray-500 transition hover:scale-95"
+            >
+              <line x1="12" y1="5" x2="12" y2="19"></line>
+              <line x1="5" y1="12" x2="19" y2="12"></line>
+            </svg>
+          </>
+        )}
       </div>
+
+      {open && (
+        <div className="absolute -top-3 right-0 z-50 w-28 translate-x-[90%] -translate-y-10 rounded-md border border-gray-300 bg-white p-2 shadow-lg">
+          <h3 className="mb-1 text-xs font-semibold text-gray-700">Select Trigger</h3>
+
+          <div
+            onClick={() => {
+              setTrigger((prev) => !prev);
+              setOpen(false);
+            }}
+            className="cursor-pointer rounded p-1 text-xs hover:bg-gray-100"
+          >
+            📧 Gmail
+          </div>
+        </div>
+      )}
+
+      <Handle type="source" position={Position.Bottom} />
     </div>
   );
 }
