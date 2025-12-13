513a8ad7a63e587930c42716fefa4ff7f9829afd
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-12-13 23:13:21 +0530

    chore: add axios and google oAuth routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import userRoute from "./routes/userRoute.js";
 import flowRoute from "./routes/flowRoute.js";
+import oAuth from "./routes/oAuth.js";
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(cors());
 app.use("/users", userRoute);
 
 app.use("/flows", flowRoute);
+
+app.use("/auth", oAuth);
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
