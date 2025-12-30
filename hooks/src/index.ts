7c4d8507bd58a5b1648bd38e2b5913839e206377
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-12-30 22:36:40 +0530

    chore: remove cors dependency from hooks service

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,18 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import flow from "./routes/flows.js";
 
 dotenv.config();
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("hi");
 });
 app.use("/flows", flow);
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
